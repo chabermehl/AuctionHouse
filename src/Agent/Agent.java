cb2e4ba87ef2b3a99387d40a3463a02ba8ddae9b
Farhang <farhang.rouhi.fr@gmail.com>
2018-11-15 12:12:38 -0700

    "Some methods are added to the agent."

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -1,4 +1,18 @@
 package Agent;
 
-public class Agent {
+import java.util.Scanner;
+
+public class Agent{
+    public static void main(String []args){
+        BankProxy bankProxy = new BankProxy();
+        int acountnum = bankProxy.createAcount(Integer.parseInt(args[1]));
+        int key = bankProxy.getKey(acountnum);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("log out")){
+            //if()
+            input=sc.nextLine();
+        }
+        bankProxy.closeAcount(acountnum);
+    }
 }
---
+++ src/Agent/BankProxy.java
@@ -0,0 +1,25 @@
+package Agent;
+
+public class BankProxy {
+    public int createAcount(int initialBalance){
+        return 0;
+    }
+    public int getKey(int acountNum){
+        return 0;
+    }
+    public boolean withdraw(int amount){
+        return false;
+    }
+    public void deposit(int amount){
+
+    }
+    public boolean lockBalance(int amount){
+        return false;
+    }
+    public void releaseLock(int amount){
+
+    }
+    public void closeAcount(int acountNum){
+
+    }
+}

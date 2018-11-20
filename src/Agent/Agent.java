3ed40f9c81f20b33823678e484aae3cc48dd6542
Alex Booher <alex.booher@yahoo.com>
2018-11-20 12:50:23 -0700

    Filled in parameters in Agent main method

--- src/Agent/Agent.java
+++ src/Agent/Agent.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Agent{
     public static void main(String []args){
         BankProxy bankProxy = new BankProxy();
-        int acountnum = bankProxy.createAcount(Integer.parseInt(args[1]));
+        int acountnum = bankProxy.createAcount(Integer.parseInt(args[1]), "localhost", false);
         int key = bankProxy.getKey(acountnum);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();

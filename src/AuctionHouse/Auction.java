5814d5d35e1ebc0b90dffa762f51e5911537fe22
Alex Booher <alex.booher@yahoo.com>
2018-11-20 14:57:38 -0700

    Starting putting in the auction house stuff

---
+++ src/AuctionHouse/Auction.java
@@ -0,0 +1,25 @@
+package AuctionHouse;
+
+/**
+ * Should only be used by the auction house
+ * stores all data related to an auction
+ */
+public class Auction {
+    private String itemName; // Name of item being sold
+    private String description; // Description of item
+    private double currentBid; // Current highest bid
+    private int bidderKey; // Key of the highest bidder
+    private double minimumBid; // Minimum bid to get the auction rolling
+
+    public Auction(String itemName, String description, double minimumBid)
+    {
+        this.itemName = itemName;
+        this.description = description;
+        this.minimumBid = minimumBid;
+    }
+
+    public double getCurrentBid () {return currentBid;}
+    public String getItemName () {return itemName;}
+    public int getBidderKey() {return bidderKey;}
+    public double getMinimumBid() {return minimumBid;}
+}
--- src/AuctionHouse/AuctionHouse.java
+++ src/AuctionHouse/AuctionHouse.java
@@ -1,13 +1,49 @@
 package AuctionHouse;
 
+import Agent.BankProxy;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class AuctionHouse {
-    public static void main(String[]args){
-        // make an instance of proxies
-        // make an account
-        // make a list of auctioned items
 
+    // All of the active auctions in this house.
+    private LinkedList<Auction> currentAuctions = new LinkedList<>();
+
+    // Bank proxy to open/close account
+    private BankProxy bankProxy;
+
+    // Info for connecting to bank
+    private String bankIP;
+    private int bankPort;
+
+    // Use this for creating house ids
+    private static int houseCount = 0;
+    private int houseID;
+
+    public static void main(String[] args)
+    {
+        AuctionHouse auctionHouse = new AuctionHouse("Insert bank ip here", 2224);
+        auctionHouse.run();
+    }
+
+    public AuctionHouse(String bankIP, int bankPort)
+    {
+        this.bankIP = bankIP;
+        this.bankPort = bankPort;
+        houseCount++;
+        houseID = houseCount;
+    }
+
+    public void run()
+    {
+        // Connect to proxy and make an account
+        // connectToBank();
+        // createBankAccount()
+
+        // Make a list of auctioned items for testing
+        currentAuctions.add(new Auction("Crab Cakes", "I made too many", 2.00));
+        currentAuctions.add(new Auction("Packet of Stevia", "This stuff is too voodoo for me", 37.00));
+        currentAuctions.add(new Auction("Bike With One Wheel", "I dunno", 18.0));
 
         // t1 = record the time
         // make a private class that has a thread which loop via a constant time and auction a new thing and check for the time
@@ -15,15 +51,19 @@ public class AuctionHouse {
         // wait for a command to terminate
     }
 
-    public synchronized void bid(int key,String name,double amount){
+    public synchronized void bid(int key,String name,double amount)
+    {
 
     }
+
     public Map<String, Double> getItems(){
         return null;
     }
-    public boolean freezAmount(int key,double amount){
+
+    public boolean freezeAmount(int key,double amount){
         return false;
     }
+
     public void acceptBid(int key,String item){
         // accept the bid from agent proxy
         // remove funds from acount

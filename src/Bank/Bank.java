f893ce772a861826b81287ad493dccdc10190975
Charles <chabermehl@unm.edu>
2018-11-27 21:40:06 -0700

    added lines to grab information coming in/leaving the server

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,9 +1,26 @@
 package Bank;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Bank {
-    public Bank(Socket agentSocket) {
+
+public class Bank implements Runnable {
+
+    private final Socket agentSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Bank(Socket agentSocket) throws IOException {
+        this.agentSocket = agentSocket;
+        out = new PrintWriter(agentSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(agentSocket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
 
     }
 

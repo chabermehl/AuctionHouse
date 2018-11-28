db5240c2566cfccd2b8b825b023aa3b395ebfb78
Charles <chabermehl@unm.edu>
2018-11-27 21:45:01 -0700

    added code to start a thread every time an agent connects

--- src/Bank/SpinUpBankServer.java
+++ src/Bank/SpinUpBankServer.java
@@ -3,6 +3,9 @@ package Bank;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SpinUpBankServer extends Application {
 
     public static void main(String[] args) {
@@ -12,5 +15,17 @@ public class SpinUpBankServer extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+    }
+
+    private void startServer(int portNumber) {
+
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+
+        while (true) {
+            Socket agentSocket = serverSocket.accept();
+            Bank bank = new Bank(agentSocket);
+            Thread t = new Thread(bank);
+            t.start();
+        }
     }
 }

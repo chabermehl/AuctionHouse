5f4e5a6dedd85bdf1b9bb59f870b4ea9cdcd2751
Charles <chabermehl@unm.edu>
2018-11-27 22:29:25 -0700

    added a gridpane for the gui

--- src/Bank/SpinUpBankServer.java
+++ src/Bank/SpinUpBankServer.java
@@ -1,7 +1,11 @@
 package Bank;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -9,16 +13,25 @@ import java.net.Socket;
 
 public class SpinUpBankServer extends Application {
 
+    private Stage window;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-
+        window = primaryStage;
+        window.setTitle("Bank Server");
+        startScene();
     }
 
     private void startScene() {
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10,10,10,10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+        TextField portNum = new TextField();
 
     }
 

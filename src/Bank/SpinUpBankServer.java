ea5cdf95c5a298d9c04763e65b0db496b011cc71
Charles <chabermehl@unm.edu>
2018-11-27 21:22:15 -0700

    added main method and start method for JavaFX gui

--- src/Bank/SpinUpBankServer.java
+++ src/Bank/SpinUpBankServer.java
@@ -1,4 +1,16 @@
 package Bank;
 
-public class SpinUpBankServer {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpinUpBankServer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }

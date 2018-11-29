592b5b6a65d348d97ee718fbe56b6b3e8e57777f
Charles <chabermehl@unm.edu>
2018-11-29 09:16:18 -0700

    serialized message object for passing messages between connections auction house and agent will need the same class in order to get messages added javadoc comments as well

--- src/Bank/Message.java
+++ src/Bank/Message.java
@@ -2,10 +2,21 @@ package Bank;
 
 import java.io.Serializable;
 
+/**
+ * This class is for passing instructions from one part of the system to the other
+ * Handles a request with the attached data
+ * Needs to serializable in order to send an object through a socket
+ * Only message objects will be passed over the connection
+ */
 public class Message implements Serializable {
     private String request;
     private String data;
 
+    /**
+     * holds data to send a message object between classes
+     * @param request action to be done be message receiver
+     * @param data data used when message is received
+     */
     public Message(String request, String data) {
         this.request = request;
         this.data = data;

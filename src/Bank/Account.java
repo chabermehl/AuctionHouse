eb1f64c2fef0eb5b04f7571169e29e64fa3afa2f
Charles <chabermehl@unm.edu>
2018-11-29 00:25:03 -0700

    added log method to be able to print out what is happening on the bank server/account end

--- src/Bank/Account.java
+++ src/Bank/Account.java
@@ -1,4 +1,21 @@
 package Bank;
 
-public class Account {
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private String name;
+    private int accountNum;
+    private double balance;
+    private int bankKey;
+
+    public Account(String name, int accountNum, double balance) {
+        this.name = name;
+        this.accountNum = accountNum;
+        this.balance = balance;
+        this.bankKey = hashCode();
+    }
+
+    public void log(String msg) {
+        System.out.println(msg);
+    }
 }

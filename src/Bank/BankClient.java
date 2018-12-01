ad7c2d1e3f1f6029d6bb662299b65f8f828450d7
Charles <chabermehl@unm.edu>
2018-12-01 08:51:48 -0700

    switched streams to be able to get objects added send message method

---
+++ src/Bank/BankClient.java
@@ -0,0 +1,39 @@
+/**
+ * Created by chabermehl
+ * 11/29/18
+ * CS 351
+ */
+package Bank;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Outward facing bank class that is interacted with
+ * Created in BankServer as a "proxy"
+ * Implements runnable as it has a socket connection
+ */
+public class BankClient implements Runnable {
+
+    private Socket agentSocket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public BankClient(Socket agentSocket) throws IOException {
+            this.agentSocket = agentSocket;
+            out = new ObjectOutputStream(agentSocket.getOutputStream());
+            in = new ObjectInputStream(agentSocket.getInputStream());
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void sendMessage(Message message) throws IOException {
+        out.writeObject(message);
+    }
+
+
+
+}

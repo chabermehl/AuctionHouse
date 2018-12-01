ad6ee9988fd209f37aa4c31a9df43aafd87f4f7c
Charles <chabermehl@unm.edu>
2018-12-01 11:03:35 -0700

    refactored BankServer class into another file so that we could initialize it from the gui and not have threading issues

--- src/Bank/BankServer.java
+++ src/Bank/BankServer.java
@@ -1,76 +1,15 @@
-/**
- * Created by chabermehl
- * 11/29/18
- * CS 351
- */
 package Bank;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+public class BankServer implements Runnable {
 
+    public int portNumber;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-/**
- * Acts as the backend of the bank
- * Opens a gui that asks for a port number to run the server on
- * Every time someone connects a new Bank thread is started in order to handle
- * that user so they do not interfere with each other
- */
-public class BankServer extends Application {
-
-    private Stage window;
-
-    public static void main(String[] args) {
-        launch(args);
+    public BankServer(int portNumber) {
+        this.portNumber = portNumber;
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        window = primaryStage;
-        window.setTitle("Bank Server");
-        startScene();
-    }
-
-    private void startScene() {
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(5);
-        grid.setHgap(5);
-        TextField portNum = new TextField();
-        portNum.setPromptText("What is the port number?");
-        portNum.setPrefColumnCount(10);
-        portNum.getText();
-        GridPane.setConstraints(portNum, 0, 0);
-        grid.getChildren().add(portNum);
-
-        Button startServerButton = new Button("Start Server");
-        startServerButton.setOnAction(event -> {
-            try {
-                startServer(Integer.parseInt(portNum.getText()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
-
-    private void startServer(int portNumber) throws IOException {
-
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-
-        while (true) {
-            Socket agentSocket = serverSocket.accept();
-            BankClient bankClient = new BankClient(agentSocket);
-            Thread t = new Thread(bankClient);
-            t.start();
-        }
+    public void run() {
 
     }
 }

b49750b62d7a5a937613ca3928adfa888b593c4c
Charles <chabermehl@unm.edu>
2018-12-01 11:21:46 -0700

    initialized new account method needs to be updtated to make an acoount numbe bank key and a bunch of other stuff

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,8 +1,19 @@
+/**
+ * Created by chabermehl
+ * 11/29/18
+ * CS 351
+ */
 package Bank;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Bank {
+    public Map<Integer, Account> AccountList = new HashMap<>();
+    public Random rand = new Random();
 
-    public int openNewAccount(String accountName) {
-
+    public int openNewAccount(String accountName, double initialDeposit) {
+        return 0;
     }
 }

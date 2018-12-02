e06e3297b0f358545289965145f1a450f1860552
Alex Booher <alex.booher@yahoo.com>
2018-12-02 16:24:52 -0700

    Moved AuctionHouseServer to new file

--- src/AuctionHouse/AuctionHouse.java
+++ src/AuctionHouse/AuctionHouse.java
@@ -28,50 +28,6 @@ public class AuctionHouse {
     private Socket bankSocket;
     private AuctionHouseServer ahServer;
 
-    /**
-     * Auction House server class used as a sort of proxy, I guess.
-     * Forwards messages to the actual auction house
-     */
-    private class AuctionHouseServer extends Thread {
-
-        private int port;
-        private AuctionHouse auctionHouse;
-        private ServerSocket serverSocket;
-
-        public AuctionHouseServer(int port, AuctionHouse ahouse) {
-            this.port = port; this.auctionHouse = ahouse;
-        }
-        public void shutdown() {
-            try {
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        // Forward message to auction house when we get a message
-        private void forwardMessageToAuctionHouse(Message message) {
-
-        }
-        public void run() {
-            try {
-                serverSocket = new ServerSocket(port);
-            } catch (IOException e) {
-                System.out.println("Problem creating auction house server on port " + port);
-                e.printStackTrace();
-            }
-
-            while (!serverSocket.isClosed()) {
-                // Try and accept new client connections
-                try {
-                    serverSocket.accept();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Server done running");
-        }
-    }
-
     public static void main(String[] args) {
         AuctionHouse auctionHouse = new AuctionHouse(0, "bankIP", 2222);
         auctionHouse.run();

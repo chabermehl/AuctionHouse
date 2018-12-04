5f226b1dd6d6482dec222af74b6cc557bca75467
Charles <chabermehl@unm.edu>
2018-12-04 12:00:15 -0700

    made the extra classes public inner classes so they could easily access newConnection flags

--- src/Agent/SerialAgentClient.java
+++ src/Agent/SerialAgentClient.java
@@ -4,6 +4,7 @@ import Bank.Message;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class SerialAgentClient implements Runnable {
@@ -63,6 +64,33 @@ public class SerialAgentClient implements Runnable {
             e.printStackTrace();
         }
     }
+    public class CreateConnection implements Runnable {
+        public String name;
+        public Socket socket;
+        public String address;
+        public int port;
+
+        BufferedReader inLine;
+
+        public CreateConnection(String name, Socket socket, String address, int port) {
+            this.name = name;
+            this.socket = socket;
+            this.address = address;
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                inLine = new BufferedReader(new InputStreamReader(System.in));
+                socket = new Socket(address, port);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     public void connectBank(String name, String address, int port) {
         bank = new CreateConnection(name, new Socket(), address, port);
@@ -74,6 +102,24 @@ public class SerialAgentClient implements Runnable {
         auction = new CreateConnection(name, new Socket(), address, port);
         Thread t = new Thread(auction);
         t.start();
+    }
+
+    public class MessageSender {
+
+        public ObjectOutputStream oos;
+        public Socket socket;
+        public String name;
+        public BufferedReader inLine;
+
+        public MessageSender(Socket socket, String name, BufferedReader inLine) throws IOException {
+            this.socket = socket;
+            this.name = name;
+            this.inLine = inLine;
+            oos = new ObjectOutputStream(socket.getOutputStream());
+        }
+    }
+
+    public class MessageReciever {
     }
 
     private void log(String msg) {

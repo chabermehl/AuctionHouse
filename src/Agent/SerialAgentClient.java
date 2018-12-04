21cc0b2498054600183a1079d78bd5f9b4f167f4
Charles <chabermehl@unm.edu>
2018-12-04 10:36:14 -0700

    added fucntionality to create an agent with a name

--- src/Agent/SerialAgentClient.java
+++ src/Agent/SerialAgentClient.java
@@ -1,4 +1,38 @@
 package Agent;
+import Bank.Message;
 
-public class SerialAgentClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SerialAgentClient extends Thread {
+
+    public BufferedReader inLine;
+    public SerialAgent agent;
+
+    public static void main(String[] args) {
+        SerialAgentClient agentClient = new SerialAgentClient();
+        agentClient.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            inLine = new BufferedReader(new InputStreamReader(System.in));
+            log("Enter Your Name: ");
+            String name = inLine.readLine();
+            agent = new SerialAgent(name);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+    private void log(String msg) {
+        System.out.println(msg);
+    }
 }

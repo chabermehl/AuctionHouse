5c61f5f939c718c13ccea9190faf16ceed9441d6
Charles <chabermehl@unm.edu>
2018-12-04 11:43:04 -0700

    added methods to allow connections to the bank and auction house easily added logic to handle if the user was connecting to the auction or bank

--- src/Agent/SerialAgentClient.java
+++ src/Agent/SerialAgentClient.java
@@ -4,11 +4,14 @@ import Bank.Message;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class SerialAgentClient implements Runnable {
 
     public BufferedReader inLine;
     public SerialAgent agent;
+    public CreateConnection bank;
+    public CreateConnection auction;
     public boolean newConnection = true;
 
     public static void main(String[] args) {
@@ -31,7 +34,28 @@ public class SerialAgentClient implements Runnable {
                             "Type \"Bank\" or \"Auction\" to connect.\n");
                     String newInLine = inLine.readLine();
                     if(newInLine.contains("Bank")) {
-                        log("Connecting to the Bank");
+                        log("What host and port is the Bank on?\n" +
+                                "<host> <port>\n");
+                        newInLine = inLine.readLine();
+                        String[] input = newInLine.split(" ");
+                        log("Connecting to the Bank...");
+                        connectBank(name, input[0], Integer.parseInt(input[1]));
+                    } else if(newInLine.contains("Auction")) {
+                        log("What host and port is the Auction on?\n" +
+                                "<host> <port>\n");
+                        newInLine = inLine.readLine();
+                        String[] input = newInLine.split(" ");
+                        log("Connecting to the Auction...");
+                        connectAuction(name, input[0], Integer.parseInt(input[1]));
+                    } else {
+                        log("Incorrect Command, Please Try Again");
+                        newConnection = true;
+                    }
+                } else {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             }
@@ -40,10 +64,17 @@ public class SerialAgentClient implements Runnable {
         }
     }
 
+    public void connectBank(String name, String address, int port) {
+        bank = new CreateConnection(name, new Socket(), address, port);
+        Thread t = new Thread(bank);
+        t.start();
+    }
 
-
-
-
+    public void connectAuction(String name, String address, int port) {
+        auction = new CreateConnection(name, new Socket(), address, port);
+        Thread t = new Thread(auction);
+        t.start();
+    }
 
     private void log(String msg) {
         System.out.println(msg);
